c8028b323e57321664d2b23ace6b1b2b92a27219
ImperialMan <1>
2021-06-09 22:01:49 +0300

    Initial commit

---
+++ NiceRedirectServer/NiceRedirectServer/Controllers/CreateRedirectController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NiceRedirectServer.Controllers
+{
+    public class CreateRedirectController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
